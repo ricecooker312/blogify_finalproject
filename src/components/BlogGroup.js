c010eb30bff0876bc113fa403e6bb162c89d2910
saihan <ricecooker312@gmail.com>
2024-10-14 17:24:27 -0500

    first commit, setting up

---
+++ src/components/BlogGroup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './css/BlogGroup.css'
+
+const BlogGroup = () => {
+    return (
+        <div class="container">
+            <div className='item item1'>1</div>
+            <div className='item item2'>2</div>
+            <div className='item item3'>3</div>
+            <div className='item item4'>4</div>
+        </div>
+    )
+}
+
+export default BlogGroup
